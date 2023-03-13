f1673fa657219dd12af4173e9c5aff1d21825d7c
pedro.lopes <pp.lopes@neki-it.com.br>
2023-03-13 11:31:47 -0300

    feat(login): teste

---
+++ src/main/java/com/example/TesteArqHookMaven/TesteArqHookMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.TesteArqHookMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteArqHookMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteArqHookMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TesteArqHookMaven/TesteArqHookMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TesteArqHookMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteArqHookMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
