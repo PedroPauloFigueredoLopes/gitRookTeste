b6020243e35ebeef0a72b47461fbf020f0f62720
pedro.lopes <pedro.lopes@neki-it.com.br>
2023-07-17 01:01:52 -0300

    teste

--- src/main/java/com/example/TesteArqHookMaven/TesteArqHookMavenApplication.java
+++ src/main/java/com/example/TesteArqHookMaven/TesteArqHookMavenApplication.java
@@ -8,8 +8,7 @@ public class TesteArqHookMavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteArqHookMavenApplication.class, args);
-		
-		//teste commitaaaaaa
+	aaaaaaaaaaaaaaaaa
 	}
 
 }
