6ffede5e42e000fc9758a380c3e6f7b9bda4a3d8
pedro.lopes <pedro.lopes@neki-it.com.br>
2023-07-19 09:31:30 -0300

    teste aprimora

--- src/main/java/com/example/TesteArqHookMaven/TesteArqHookMavenApplication.java
+++ src/main/java/com/example/TesteArqHookMaven/TesteArqHookMavenApplication.java
@@ -8,7 +8,7 @@ public class TesteArqHookMavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteArqHookMavenApplication.class, args);
-	
+	//aprimora neki
 	}
 
 }
